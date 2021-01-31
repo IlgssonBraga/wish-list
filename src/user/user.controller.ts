bbfbef651ad4860f4876ee850b233ae3b00039cd
Ilgsson Braga <ilgssonbraga@localhost.localdomain>
2021-01-31 15:28:00 -0300

    feat: User CRUD

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,18 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
 import { User } from './user.entity';
 import { UserService } from './user.service';
 import { CreateUserDto } from './user.service';
+
 
 @Controller('/users')
 export class UserController {
@@ -9,13 +20,36 @@ export class UserController {
 
   @Get()
   async index(): Promise<User[]> {
-    return this.userService.findAll();
+    return await this.userService.findAll();
   }
 
   @Post()
   async store(@Body() createUserDto: CreateUserDto): Promise<User> {
-    const user = this.userService.insertOne(createUserDto);
+    const user = await this.userService.insertOne(createUserDto);
 
     return user;
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() createUserDto: CreateUserDto,
+  ): Promise<User> {
+    const user = await this.userService.updateOne(id, createUserDto);
+
+    return user;
+  }
+
+  @Get(':id')
+  async show(@Param('id') id: number): Promise<User> {
+    const user = await this.userService.findOne(id);
+
+    return user;
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  async delete(@Param('id') id: number): Promise<void> {
+    await this.userService.deleteOne(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,7 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, HttpException, HttpStatus } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { hashSync } from 'bcryptjs';
 
 export interface CreateUserDto {
   name: string;
@@ -16,17 +17,58 @@ export class UserService {
   ) {}
 
   async findAll(): Promise<User[]> {
-    return this.userRepository.find();
+    const users = await this.userRepository.find();
+
+    users.map((user) => delete user.password);
+
+    return users;
+  }
+
+  async findOne(id: number): Promise<User> {
+    const user = await this.userRepository.findOneOrFail(id);
+
+    delete user.password;
+
+    return user;
+  }
+
+  async updateOne(
+    id: number,
+    { name, email, password }: CreateUserDto,
+  ): Promise<User> {
+    await this.userRepository.findOneOrFail(id);
+    const newPassword = hashSync(password, 8);
+
+    await this.userRepository.update(
+      { id },
+      { name, email, password: newPassword },
+    );
+
+    const newUser = await this.userRepository.findOne(id);
+
+    delete newUser.password;
+
+    return newUser;
   }
 
   async insertOne({ name, email, password }: CreateUserDto): Promise<User> {
+    const newPassword = hashSync(password, 8);
+
     const user = this.userRepository.create({
       name,
       email,
-      password,
+      password: newPassword,
     });
 
     await this.userRepository.save(user);
+
+    delete user.password;
+
     return user;
+  }
+
+  async deleteOne(id: number): Promise<void> {
+    await this.userRepository.findOneOrFail(id);
+    this.userRepository.delete(id);
   }
 }
